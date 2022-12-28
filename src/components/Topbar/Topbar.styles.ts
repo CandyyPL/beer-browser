3bfcb82c178a4725d32a001ec8340070c9f87314
CandyyPL <aplay58.yt@gmail.com>
2022-12-28 12:35:44 +0100

    Made favorite content count

--- src/components/Topbar/Topbar.styles.ts
+++ src/components/Topbar/Topbar.styles.ts
@@ -8,11 +8,85 @@ export const TopbarWrapper = styled.div`
   box-shadow: 0 0 20px 1px rgba(0, 0, 0, 0.4);
 
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
 
   font-size: 40px;
   font-family: 'Nunito', sans-serif;
   font-weight: bold;
   color: #111;
+
+  .left,
+  .middle,
+  .right {
+    width: 30%;
+    height: 100%;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    font-family: 'Nunito', sans-serif;
+    color: #111;
+  }
+
+  .middle {
+    font-size: 40px;
+    font-weight: bold;
+  }
+
+  .right {
+    font-size: 24px;
+    font-weight: 500;
+
+    .favCount {
+      width: 30px;
+      height: 40px;
+
+      background-color: #ddd;
+
+      border: none;
+      border-radius: 5px;
+      padding: 10px;
+      margin: 10px;
+
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      opacity: 0;
+      transition: opacity 0.2s;
+
+      &.active {
+        opacity: 1;
+      }
+    }
+  }
+`
+
+export const Favorite = styled.span`
+  cursor: pointer;
+
+  position: relative;
+
+  &::after {
+    content: '';
+
+    width: 100%;
+    height: 2px;
+
+    background-color: #111;
+
+    position: absolute;
+    bottom: 0;
+    left: 0;
+
+    transform: scaleX(0);
+    transform-origin: 0%;
+    transition: transform 0.3s;
+  }
+
+  &:hover::after {
+    transform: scaleX(1);
+  }
 `
--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -1,10 +1,23 @@
-import { TopbarWrapper } from '@/components/Topbar/Topbar.styles'
-import { FC } from 'react'
+import { Favorite, TopbarWrapper } from '@/components/Topbar/Topbar.styles'
+import { ContentContext } from '@/providers/ContentProvider'
+import { IContentContext } from '@/types/Beer'
+import { FC, useContext } from 'react'
 
 const Topbar: FC = () => {
+  const { fav } = useContext<IContentContext>(ContentContext)
+
   return (
     <TopbarWrapper>
-      <span>BEER BROWSER</span>
+      <div className='left' />
+      <div className='middle'>
+        <span>BEER BROWSER</span>
+      </div>
+      <div className='right'>
+        <Favorite>Favorites</Favorite>
+        <div className={`favCount ${fav.length > 0 ? 'active' : ''}`}>
+          {fav.length > 0 ? fav.length : '1'}
+        </div>
+      </div>
     </TopbarWrapper>
   )
 }
